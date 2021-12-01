c7cb63db3c0d75cf9fb73edb2a0ac8484ead6ac1
Jordan Sam <jsam007@sledge.cs.ucr.edu>
2021-11-30 23:42:16 -0800

    Moddified shm_open and shm_close functions

--- Makefile
+++ Makefile
@@ -177,6 +177,7 @@ UPROGS=\
 	_zombie\
 	_shm_cnt\
 	_null\
+	_shm_cnt\
 
 fs.img: mkfs README $(UPROGS)
 	./mkfs fs.img README $(UPROGS)
--- shm.c
+++ shm.c
@@ -30,20 +30,82 @@ void shminit() {
 
 int shm_open(int id, char **pointer) {
 
-//you write this
+//you write this LAB 4 MODIFIED
 
+    char exist = 0;     //checks for the existance of the id
+    int i = 0;          //index page
 
+    acquire(&(shm_table.lock)); //get the lock for the shm table
 
+    //CASE 1 WHERE ID EXISTS ALREADY
 
-return 0; //added to remove compiler warning -- you should decide what to return
+    //start by looping through the table
+    for(i= 0; i < 64; ++i){         //If the id matches the id of the page table, check that it was case 1 and then exit
+        if(shm_table.shm_pages[i].id == id){
+            exist = 1;
+            break;
+        }
+    }
+
+    //CASE 2 WHERE SHARED MEM SEG DNE
+    if(!exist){
+        for(i = 0; i < 64; ++i){                        //loop through the table to find the page again
+            if(shm_table.shm_pages[i].id == 0){
+                char *page = kalloc();                  //allocate a new page
+                memset(page, 0, PGSIZE);                //MAP TO 0: initialize + write
+                shm_table.shm_pages[i].frame = page;    //New page gets assigned the fram
+                shm_table.shm_pages[i].id = id;         //page gets ID and exit
+                break;
+            }
+        }
+    }
+
+    //NEXT: we need to map the id to the memory in the page table
+    //Here we reference mmu.h
+    mappages(
+            myproc()->pgdir,                     //getting the pointer of pagedir
+            (void *)PGROUNDUP(myproc()->sz),     //gets the virtual adrr of page
+            PGSIZE,                              //size of mapping
+            V2P(shm_table.shm_pages[i].frame),   //gets the physical address to point to
+            (PTE_W | PTE_U)                      //sets the permissions writtable and accessable to user
+            );
+
+    //NEXT: we need to increment the reference count and have a pointer to the virtual address
+    shm_table.shm_pages[i].refcnt++;    
+    *pointer = (char *)PGROUNDUP(myproc()->sz);        
+
+    //Current process needs to be updated and we need to then unlock shm table
+
+    myproc()->sz += PGSIZE;
+    release(&(shm_table.lock));
+
+return 0;
 }
 
-
 int shm_close(int id) {
-//you write this too!
 
+//you write this too! LAB 4 MODIFIED
 
+int i;
 
+//start by looping through the table and check if the ID is found
+//While in the for loop, check if the memory is shared and decrement the reference count.
+for(i = 0; i < 64; ++i){
+    if(shm_table.shm_pages[i].id == id){
+        if(shm_table.shm_pages[i].refcnt > 1){
+            --shm_table.shm_pages[i].refcnt;
+        }
 
-return 0; //added to remove compiler warning -- you should decide what to return
+        //Set the values to 0 to release the pages
+
+        else{
+            shm_table.shm_pages[i].id = 0;
+            shm_table.shm_pages[i].frame = 0;
+            shm_table.shm_pages[i].refcnt = 0;
+        }
+        break;
+    }
+}
+
+return 0;
 }
